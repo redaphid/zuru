ed7fab99592b6fa9961059f5bf3e287c5adefe8d
Aaron Herres <iam@aaronherres.com>
2025-04-28 23:02:58 -0700

    moving shader to props

--- Zuru.mjs
+++ Zuru.mjs
@@ -1,12 +1,23 @@
 import { html } from "./html.mjs"
+import { useEffect, useState, useRef } from "preact/hooks"
 import { make } from "wish"
 
-export const Zuru = () => {
-  return html`
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  `
+export const Zuru = ({ shader }) => {
+  const [render, setRender] = useState(null)
+  const canvas = useRef(null)
+  useEffect(async () => {
+    if (!shader) return
+    if (!canvas.current) return
+    const render = await make({
+      canvas: canvas.current,
+      fragmentShader: shader,
+    })
+    setRender(() => render)
+  }, [shader, canvas])
+  useEffect(() => {
+    render()
+  }, [render])
+  return html` <canvas ref=${canvas}></canvas> `
 }
 
 export default Zuru
